1c06fc0d5413f88ec5e05ce38b4d6c885be38416
Shahin Azizi <shahin@localhost.localdomain>
2024-09-05 23:51:35 +0330

    add two new routes

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/4353", (req, res) => res.send("Express on Vercel"));
+app.get("/35345", (req, res) => res.send("Express on Vercel"));
 
 export default app;
